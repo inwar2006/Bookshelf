e5e1cf643bbe39adaba22d589277cdbb324d824d
Igor Pozhaluev <inwar2006@gmail.com>
2020-04-14 22:53:26 +0300

    Added list of books

--- app/src/main/java/com/inwar2006/bookshelf/FindBookActivity.java
+++ app/src/main/java/com/inwar2006/bookshelf/FindBookActivity.java
@@ -5,8 +5,11 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Spinner;
 import android.widget.TextView;
+import java.util.List;
 
 public class FindBookActivity extends AppCompatActivity {
+
+    private BookExpert bookExpert = new BookExpert();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -15,9 +18,14 @@ public class FindBookActivity extends AppCompatActivity {
     }
 
     public void onClickFindBook(View view){
-        TextView kinds = (TextView) findViewById(R.id.kind);
+        TextView bookMonitor = (TextView) findViewById(R.id.monitor);
         Spinner type = (Spinner) findViewById(R.id.directions);
         String bookType = String.valueOf(type.getSelectedItem());
-        kinds.setText(bookType);
+        List<String> typeList = bookExpert.getTypes(bookType);
+        StringBuilder typesFormatted = new StringBuilder();
+        for(String book: typeList){
+            typesFormatted.append(book).append('\n');
+        }
+        bookMonitor.setText(typesFormatted);
     }
 }
